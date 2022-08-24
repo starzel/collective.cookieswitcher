74ad115a5144d352dc00203aa8909f1b343c463d
Steffen Lindner <mail@steffen-lindner.de>
2022-08-24 16:21:30 +0200

    Add full translation support

--- src/collective/cookieswitcher/viewlets/cookieswitcher.py
+++ src/collective/cookieswitcher/viewlets/cookieswitcher.py
@@ -1,7 +1,33 @@
 # -*- coding: utf-8 -*-
 
+from AccessControl.unauthorized import Unauthorized
 from plone.app.layout.viewlets import ViewletBase
+from plone.app.multilingual.interfaces import ITranslationManager
 from plone import api
+
+def get_link(path):
+    """
+    Translates given path in correct lang link
+    """
+    if '@@multilingual-universal-link' in path:
+        return path
+    try:
+        canonical_target = api.content.get(path=path)
+    except Unauthorized:
+        return
+    if not canonical_target:
+        return
+    tm = ITranslationManager(canonical_target, None)
+    if not tm:
+        return canonical_target.absolute_url()
+    lang = api.portal.get_current_language()
+    try:
+        translation = tm.get_translation(lang)
+    except Unauthorized:
+        return
+    if translation:
+        if api.user.has_permission('View', obj=translation):
+            return translation.absolute_url()
 
 class CookieSwitcherViewlet(ViewletBase):
 
@@ -11,3 +37,6 @@ class CookieSwitcherViewlet(ViewletBase):
 
     def render(self):
         return super(CookieSwitcherViewlet, self).render()
+
+    def page_link(self):
+        return get_link(path='/de/datenschutz')
